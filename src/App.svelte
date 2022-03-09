63de10de92fbb643fa139266f6d91738328c67c7
Nafiz Mahmud <91520170+Nafiz-Mahmud@users.noreply.github.com>
2022-03-10 00:52:05 +0600

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	let newTodo = ''
+	let todos  =[]
+
+let newId = 0
+let count = 0
+
+const addTodo =()=>{
+	newId++
+	todos  = [...todos,{text:newTodo,id:newId}]
+	newTodo = ''
+}
+
+
+
+
+
+</script>
+
+
+
+<main>
+	<h1>My first TODO app in SVELTE</h1>
+	
+	<input type="text" placeholder="New todo" bind:value={newTodo} >
+	<button on:click={addTodo} >Add todo</button>
+{#if todos.length>0 }
+
+{#each todos as todo (todo.id) }
+	<h2>{todo.text}  </h2>
+	
+	<!-- <button  >Delete</button> -->
+{/each}
+
+{:else}
+	<h3>There are no Todo</h3>
+{/if}
+
+</main>
+<style>
+
+</style>
